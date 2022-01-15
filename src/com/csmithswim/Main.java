d2483e8819fc02847e202c023414d16a9239b2e4
Cullen S <cullensmi@gmail.com>
2022-01-15 09:34:09 -0500

    first commit

---
+++ src/com/csmithswim/Main.java
@@ -0,0 +1,11 @@
+package com.csmithswim;
+import java.util.*;
+
+public class Main {
+
+    //Input String
+    //Out String
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/csmithswim/Solutions.java
@@ -0,0 +1,12 @@
+package com.csmithswim;
+
+public class Solutions {
+
+
+
+    public static void lastAndSecondLast(String input) {
+        System.out.println(input.substring(input.length() - 1) + " " + input.substring(input.length() - 2, input.length() - 1));
+    }
+
+
+}
