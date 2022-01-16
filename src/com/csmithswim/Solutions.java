27c0f69e19d710b44c460ece31a684ff4a4fd359
Cullen S <cullensmi@gmail.com>
2022-01-15 22:52:35 -0500

    solved pricecheck algorithm

--- src/com/csmithswim/Solutions.java
+++ src/com/csmithswim/Solutions.java
@@ -1,6 +1,47 @@
 package com.csmithswim;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solutions {
+
+    public static void priceCheck() {
+        //input - 4 string arrays
+        //output - int(sum of errors)
+
+        //instantiate variables(different products, prices, sold products, etc.)
+        //make a hashmap of products/prices
+        //iterate through items sold
+        //IF a sold item's price != inventory product's price
+        //sum++
+        //return error
+
+        //Sample input 0
+        String[] products = {"rice", "sugar", "wheat", "cheese"};
+        double[] productPrices = {16.89, 59.92, 20.89, 345.99};
+        String[] productSold = {"rice", "cheese"};
+        double[] soldPrice = {18.99, 400.89};
+
+//        String[] products = {"chocolate", "cheese", "tomato"};
+//        double[] productPrices = {15.00, 300.90, 23.44};
+//        String[] productSold = {"chocolate", "cheese", "tomato"};
+//        double[] soldPrice = {15, 300.90, 10.00};
+
+        Map<String, Double> priceList = new HashMap<>();
+
+        int pricingErrors = 0;
+
+        for (int i = 0; i < products.length; i++) {
+            priceList.put(products[i], productPrices[i]);
+        }
+
+        for (int i = 0; i < productSold.length; i++) {
+            if (priceList.get(productSold[i]) != soldPrice[i]) {
+                pricingErrors++;
+            }
+        }
+        System.out.println(pricingErrors);
+    }
 
     public static void lastAndSecondLast(String input) {
         System.out.println(input.substring(input.length() - 1) + " " + input.substring(input.length() - 2, input.length() - 1));
